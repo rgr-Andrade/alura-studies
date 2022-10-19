97944b868a80f18c7e5c5022122a6d297923db88
Rogério Andrade <rgrandrade@hotmail.com>
2022-10-18 21:11:22 -0300

    Criação dos componentes de botão, lista, formulário e estilização

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+class Botao extends React.Component {
+  render() {
+     return (
+       <button className='botao'>
+           Botão
+         </button>
+        )
+     }
+    }
+
+    export default Botao;
---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './style.scss';
+
+function Lista(){
+  const tarefas = [{
+    tarefa: 'Reactt',
+    tempo: '02:00:00'
+  },{
+    tarefa: 'JavaScript',
+    tempo: '01:00:00'
+  },{
+    tarefa:'TypeScript',
+    tempo:'03:00:00'
+  }]
+
+  return(
+    <aside className='listaTarefas'>
+      <h2>Estudo do dia</h2>
+      <ul>
+
+        {tarefas.map((item, index)=>(
+          <li key={index} className='item '>
+            <h3> {item.tarefa} </h3>
+            <span> {item.tempo} </span>
+          </li>
+        ))}
+
+      </ul>
+    </aside>
+  )
+}
+
+export default Lista;
